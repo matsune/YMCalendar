3d3a916c676b914844d3b805d45f98c893221663
松根佑眞 <yuma.matsune@gmail.com>
2017-03-10 22:46:14 +0900

    Initial Commit

---
+++ YMCalendar/YMCalendar.h
@@ -0,0 +1,19 @@
+//
+//  YMCalendar.h
+//  YMCalendar
+//
+//  Created by Yuma Matsune on 2017/03/10.
+//  Copyright © 2017年 Yuma Matsune. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YMCalendar.
+FOUNDATION_EXPORT double YMCalendarVersionNumber;
+
+//! Project version string for YMCalendar.
+FOUNDATION_EXPORT const unsigned char YMCalendarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YMCalendar/PublicHeader.h>
+
+
